3bb1787b6e316ebae6d4124248c03bdce8ced2a7
Roger King <10982875+roger-king@users.noreply.github.com>
2022-03-15 21:05:56 -0400

    feat: host in constants

---
+++ src/constants.ts
@@ -0,0 +1,22 @@
+export const hosts = [
+  {
+    firstName: "roger",
+    lastName: "king",
+    bio: "Roger has 10+ years of software engineering experience. Currently, Roger is working at a tech-fitness company called Balanced as their founding engineer.",
+  },
+  {
+    firstName: "joseph",
+    lastName: "micceri",
+    bio: "",
+  },
+  {
+    firstName: "george",
+    lastName: "markantonis",
+    bio: "",
+  },
+  {
+    firstName: "greg",
+    lastName: "meade",
+    bio: "",
+  },
+];
