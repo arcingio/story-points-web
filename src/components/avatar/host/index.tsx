b0c0b5d1ea1df800e8a7aae5d74c04967ede7603
Roger King <10982875+roger-king@users.noreply.github.com>
2022-03-15 21:07:06 -0400

    refactor: better prop type for host

--- src/components/avatar/host/index.tsx
+++ src/components/avatar/host/index.tsx
@@ -2,7 +2,15 @@ import { Box, Heading } from "@chakra-ui/react";
 import Image from "next/image";
 import { HostInfo } from "../../../types";
 
-export const HostAvatar: React.FC<HostInfo> = ({ firstName }) => {
+interface HostAvatarProps {
+  host: HostInfo;
+  withLastName: boolean;
+}
+
+export const HostAvatar: React.FC<HostAvatarProps> = ({
+  host: { firstName, lastName },
+  withLastName,
+}) => {
   return (
     <Box display="flex" flexDirection="column" alignItems="center">
       <Image
@@ -11,7 +19,7 @@ export const HostAvatar: React.FC<HostInfo> = ({ firstName }) => {
         height="150px"
       />
       <Heading size="md" textTransform="capitalize">
-        {firstName}
+        {firstName} {withLastName && lastName}
       </Heading>
     </Box>
   );
