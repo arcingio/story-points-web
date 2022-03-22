49f9ff7d03e1ecbbfd20712a39a5189e75c6e433
Roger King <10982875+roger-king@users.noreply.github.com>
2022-03-21 22:10:07 -0400

    fix: podcast icons to route to podcast page

--- src/components/podcast-platform/index.tsx
+++ src/components/podcast-platform/index.tsx
@@ -1,11 +1,24 @@
-import Image from "next/image";
+import { Image } from "@chakra-ui/react";
 
 interface PodcastPlatformProps {
   platform: string;
+  link: string;
 }
 
 export const PodcastPlatform: React.FC<PodcastPlatformProps> = ({
   platform,
+  link,
 }) => {
-  return <Image src={`/images/${platform}.png`} width="50px" height="50px" />;
+  return (
+    <Image
+      src={`/images/${platform}.png`}
+      width={["35px", "50px"]}
+      height={["35px", "50px"]}
+      onClick={() => {
+        window.open(link, "_blank");
+      }}
+      alt=""
+      cursor="pointer"
+    />
+  );
 };
