5de7112454728b77f89554230cb82d8d9fbf033b
Roger King <10982875+roger-king@users.noreply.github.com>
2022-03-21 22:10:16 -0400

    feat: adding podcast links

--- src/components/podcast-platform/list.tsx
+++ src/components/podcast-platform/list.tsx
@@ -1,13 +1,23 @@
 import { Box } from "@chakra-ui/react";
 import { PodcastPlatform } from ".";
+import { applePodcastURL, spotifyURL } from "../../constants";
 
 export const ListOfPodcatPlatforms: React.FC = () => {
-  const platforms = ["spotify", "podcast"];
+  const platforms = [
+    {
+      name: "spotify",
+      link: spotifyURL,
+    },
+    {
+      name: "podcast",
+      link: applePodcastURL,
+    },
+  ];
 
   return (
     <Box gap="6" display="flex">
       {platforms.map((p) => {
-        return <PodcastPlatform key={p} platform={p} />;
+        return <PodcastPlatform key={p.name} platform={p.name} link={p.link} />;
       })}
     </Box>
   );
